d1f77819a5ebd9b10bc8932c1b3bc1778d6e96d8
Нелли <nellibelli7@gmail.com>
2026-06-02 22:31:38 +0300

    Add Swagger documentation

--- Source/backend/src/main.ts
+++ Source/backend/src/main.ts
@@ -1,8 +1,38 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Tree Park Management API')
+    .setDescription('API системы учета деревьев')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+
+  SwaggerModule.setup(
+    'api',
+    app,
+    document,
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
